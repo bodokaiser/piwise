ad600c4d0b7a684a540eb09fbb25a8687c1a1b36
Bodo Kaiser <i@bodokaiser.io>
2017-03-21 16:59:40 +0100

    added image visualization with visdom

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import argparse
+import numpy as np
+import visdom
 
 from piwise import network, dataset
 
 from torch import nn, optim, autograd
 from torch.utils import data
-from torchvision import transforms
+from torchvision import utils, transforms
+
+def vis_image(vis, image, epoch, step, name):
+    if image.is_cuda:
+        image = tensor.cpu()
+    if isinstance(image, autograd.Variable):
+        image = image.data
+    image = image.numpy()
+
+    if len(image.shape) == 3:
+        if image.shape[0] == 3:
+            image = image.transpose((1, 2, 0))
+        if image.shape[0] == 1:
+            image = image[0].transpose((0, 1))
+    if image.dtype == np.int64:
+        image = image.astype(np.uint8)
+
+    vis.image(image,
+        opts=dict(title=f'{name} (epoch: {epoch}, step: {step})'))
 
 def main(args):
     model = network.Basic()
@@ -21,13 +41,16 @@ def main(args):
             transforms.CenterCrop(256),
             transforms.ToTensor(),
             transforms.Lambda(lambda t: t.mul(255).long()[0]),
-        ])), shuffle=True, num_workers=args.num_workers)
+        ])), num_workers=args.num_workers, batch_size=args.batch_size)
 
     optimizer = optim.Adam(model.parameters())
     criterion = network.CrossEntropySoftmax2d()
 
+    if args.visdom:
+        vis = visdom.Visdom()
+
     for epoch in range(args.num_epochs+1):
-        epoch_loss = 0
+        epoch_loss = []
 
         for step, (images, labels) in enumerate(loader):
             if args.cuda:
@@ -42,18 +65,23 @@ def main(args):
             loss.backward()
             optimizer.step()
 
-            epoch_loss += loss.data[0]
+            epoch_loss.append(loss.data[0])
 
-        print(f'epoch: {epoch}, loss: {epoch_loss}')
+            if args.visdom and step % args.visdom_steps == 0:
+                vis_image(vis, inputs[0], epoch, step, 'input')
+                vis_image(vis, outputs[0], epoch, step, 'output')
+                vis_image(vis, targets[0], epoch, step, 'target')
 
-
-
+        print(f'epoch: {epoch}, epoch_loss: {sum(epoch_loss)}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cuda', action='store_true')
+    parser.add_argument('--visdom', action='store_true')
+    parser.add_argument('--visdom-steps', type=int, default=10)
     parser.add_argument('--num-epochs', type=int, default=5)
     parser.add_argument('--num-workers', type=int, default=2)
+    parser.add_argument('--batch-size', type=int, default=1)
     parser.add_argument('--dataroot', nargs='?', default='data')
     parser.add_argument('--trainroot', nargs='?', default='train')
     main(parser.parse_args())
